a8ae0358e6f49166894de4ef63f2fbe9c1281cd3
moula2002 <iammoulahussain@gmail.com>
2026-03-10 15:56:47 +0530

    changes 1

--- backend/server.js
+++ backend/server.js
@@ -25,6 +25,7 @@ const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 465,
   secure: true,
+   family: 4, 
   connectionTimeout: 10000,
   greetingTimeout: 10000,
   socketTimeout: 10000,
