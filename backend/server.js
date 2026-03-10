126a852d7eb668807fc6f09dfdb09c999b49e48b
moula2002 <iammoulahussain@gmail.com>
2026-03-10 15:23:24 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,9 @@ app.use(express.json({ limit: "50mb" }));
 // ==========================
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
